9ccb49d441e8fc08f296e33b9663a5f802301859
Alireza <alirezaghafari0101@gmail.com>
2023-07-25 15:43:07 +0330

    add toolkit to app, create store and add function for getting users from server

---
+++ src/Redux/slices/userSlices.js
@@ -0,0 +1,25 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getUsersFromServer = createAsyncThunk(
+  "users/getUsersFromServer",
+  async (url) => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+  }
+);
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: [],
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(getUsersFromServer.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.push(...action.payload);
+    });
+  },
+});
+
+export default usersSlice.reducer;
